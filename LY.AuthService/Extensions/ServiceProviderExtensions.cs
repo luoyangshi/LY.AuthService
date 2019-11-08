e32233f50a571083499ca3fd835b0b214d352573
曹武贝 <NETCORE@DESKTOP-HT7CCU5>
2019-11-08 16:28:38 +0800

    添加项目文件。

---
+++ LY.AuthService/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace LY.AuthService.Extensions
+{
+    public static class ServiceProviderExtensions
+    {
+        public static IApplicationBuilder UseInitServiceProvider(this IApplicationBuilder app)
+        {
+            AppGlobalVariable.ServiceProvider = app.ApplicationServices;
+            return app;
+        }
+    }
+
+    public static class AppGlobalVariable
+    {
+        public static IServiceProvider ServiceProvider;
+    }
+}
---
+++ LY.AuthService/InMemoryConfig.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using LY.AuthService.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LY.AuthService
+{
+    public static class InMemoryConfig
+    {
+        public static List<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("AuthService","授权服务"),
+                new ApiResource("OrderService","订单服务")
+            };
+        }
+
+        public static List<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>()
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
+            };
+        }
+
+        public static List<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId = "pwd",
+                    ClientName = "密码模式",
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowOfflineAccess = true,
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "AuthService",
+                        "OrderService"
+                    }
+                },
+                new Client()
+                {
+                    ClientId = "mvc",
+                    ClientName = "授权模式",
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    RedirectUris = {"http://localhost:5003/signin-oidc"},
+                    PostLogoutRedirectUris = {"http://localhost:5003/signout-callback-oidc"},
+                    AllowAccessTokensViaBrowser = true,
+                    AllowOfflineAccess = true,
+                    RequireConsent = false,
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "AuthService",
+                        "OrderService"
+                    }
+
+                }
+            };
+        }
+
+        public static List<TestUser> GeTestUsers()
+        {
+            return new List<TestUser>()
+            {
+                new TestUser()
+                {
+                    SubjectId = "001",
+                    Username = "smallwall",
+                    Password = "123123",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.PhoneNumber,"15100000000")
+                    }
+                }
+            };
+        }
+    }
+}

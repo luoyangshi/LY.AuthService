f3cb51f4e761152b152f799f59608ff5b69cc4bb
曹武贝 <NETCORE@DESKTOP-HT7CCU5>
2019-11-09 09:36:56 +0800

    检测当前环境变量

---
+++ LY.AuthService/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LY.AuthService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        /// <summary>
+        /// 获取当前环境变量
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetEnvironment")]
+        public string GetEnvironment()
+        {
+            return _webHostEnvironment.EnvironmentName;
+        }
+    }
+}

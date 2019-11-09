b869082508bad1ec33610097f6efba71942adeea
曹武贝 <NETCORE@DESKTOP-HT7CCU5>
2019-11-09 17:14:11 +0800

    health

---
+++ LY.AuthService/Controllers/HealthController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LY.AuthService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        /// <summary>
+        /// 健康检查
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok("ok");
+        }
+    }
+}
